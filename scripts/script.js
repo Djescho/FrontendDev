2110966164d5c69003f9163953a0f794b9912c79
Djescho <70486952+Djescho@users.noreply.github.com>
2020-09-23 23:31:31 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+var menuOpen = document.querySelector("#menuButton");
+var closeMenu = document.querySelector(".closeButton")
+var sideMenu = document.querySelector(".sideMenu");
+
+function toggleMenu() {
+    console.log("Menu toggle");
+    sideMenu.classList.toggle("sideMenuOpen");
+}
+
+menuOpen.addEventListener('click', toggleMenu);
+closeMenu.addEventListener('click', toggleMenu);
